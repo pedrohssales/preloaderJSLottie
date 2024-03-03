93863ea40085374300c3ed1cec7948c8b81ef1db
pedrohssales <130273434+pedrohssales@users.noreply.github.com>
2024-03-03 03:06:29 -0300

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -1,32 +1,25 @@
 const lottieContainer = document.createElement('div');
-lottieContainer.id = 'lottieContainer';
 lottieContainer.style.cssText = `
-    max-width: 100%;
-    max-height: 100%;
-    display: none;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(255, 255, 255, 0.7);
+    z-index: 999;
 `;
-lottieContainer.style.display = 'none';
 
 document.body.appendChild(lottieContainer);
 
-function showLottie() {
-    lottieContainer.style.display = 'block';
-    // Substitua pela URL direta do seu arquivo JSON do Lottie.
+window.addEventListener('load', () => {
     lottie.loadAnimation({
         container: lottieContainer,
-        renderer: 'svg', // ou 'canvas' se preferir
+        renderer: 'svg',
         loop: true,
         autoplay: true,
-        path: 'https://pedrohssales.github.io/preloaderJSLottie/carregamento-cmeax-novo.json',
+        path: 'https://pedrohssales.github.io/preloaderJSLottie/carregamento-cmeax.json',
     });
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-    showLottie(); // Teste sem o setTimeout
 });
-
-window.addEventListener('load', showLottie);
