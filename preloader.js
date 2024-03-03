783c9632b376aa236509155ecf445d8a9b389c3c
pedrohssales <130273434+pedrohssales@users.noreply.github.com>
2024-03-03 02:51:31 -0300

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -18,29 +18,32 @@ lottieContainer.id = 'lottieContainer';
 lottieContainer.style.cssText = `
     max-width: 100%;
     max-height: 100%;
-    display: none; // Inicialmente oculto
+    display: none;
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
 `;
+lottieContainer.style.display = 'none';
 
 document.body.appendChild(overlay);
 document.body.appendChild(lottieContainer);
 
 function hideOverlay() {
     overlay.style.display = 'none';
-    lottieContainer.style.display = 'block'; // Mostrar após o carregamento
+    lottieContainer.style.display = 'block';
+    // Substitua pela URL direta do seu arquivo JSON do Lottie.
     lottie.loadAnimation({
         container: lottieContainer,
-        renderer: 'svg',
+        renderer: 'svg', // ou 'canvas' se preferir
         loop: true,
         autoplay: true,
         path: 'https://pedrohssales.github.io/preloaderJSLottie/carregamento-cmeax.json',
     });
 }
 
-// Remova o eventListener para 'DOMContentLoaded' se não for necessário
-// document.addEventListener('DOMContentLoaded', hideOverlay);
+document.addEventListener('DOMContentLoaded', () => {
+    hideOverlay(); // Teste sem o setTimeout
+});
 
 window.addEventListener('load', hideOverlay);
